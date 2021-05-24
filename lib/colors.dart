aa52452190aca058f4df8ae9ecc30af65022fd89
suragch <studymongolian@gmail.com>
2021-05-24 12:04:40 +0800

    beginning of accessing device sensors lesson

--- lib/colors.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-class CustomColors {
-  static const Color android = Colors.blue;
-}
--- lib/main.dart
+++ lib/main.dart
@@ -13,44 +13,22 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
+  const HomePage({Key? key}): super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    Size screenSize = MediaQuery.of(context).size;
 
     return Scaffold(
-      body: Row(
+      body: ListView(
         children: [
-          Expanded(
-            flex: 2,
-            child: LayoutBuilder(
-              builder: (context, constraints) => Container(
-                color: Colors.deepPurple,
-                child: Center(
-                  child: Text(
-                    'View 1\n\n' +
-                        '[MediaQuery]:\n ${screenSize.width.toStringAsFixed(2)}\n\n' +
-                        '[LayoutBuilder]:\n${constraints.maxWidth.toStringAsFixed(2)}',
-                    style: TextStyle(color: Colors.white, fontSize: 18),
-                  ),
-                ),
-              ),
-            ),
+          ListTile(
+            title: Text('Camera demo'),
           ),
-          Expanded(
-            flex: 1,
-            child: LayoutBuilder(
-              builder: (context, constraints) => Container(
-                color: Colors.white,
-                child: Center(
-                  child: Text(
-                    'View 2\n\n' +
-                        '[MediaQuery]:\n ${screenSize.width.toStringAsFixed(2)}\n\n' +
-                        '[LayoutBuilder]:\n${constraints.maxWidth.toStringAsFixed(2)}',
-                    style: TextStyle(color: Colors.blue, fontSize: 18),
-                  ),
-                ),
-              ),
-            ),
+          ListTile(
+            title: Text('Microphone demo'),
+          ),
+          ListTile(
+            title: Text('GPS demo'),
           ),
         ],
       ),
